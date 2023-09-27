c81e5b08df5883d143e0ad5e527ef0d711829425
Rai Joia <megac154@gmail.com>
2023-09-27 20:27:06 -0300

    refactor inicio div for main and AboutMe text

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,7 +1,13 @@
+import InicioRight from "../InicioRight"
+
 const AboutMe = () => {
   return (
-    <h3>Sobre mim</h3>
-    
+    <>
+      <h3 className="text-center text-3xl" >Sobre mim</h3>
+      <main className="md:grid md:grid-cols-2 justify-center gap-40  pt-44 pb-60 ml-28 mr-28">
+        <InicioRight />
+      </main>
+    </>
   )
 }
 
--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -3,10 +3,10 @@ import InicioRight from "../../components/InicioRight"
 
 const Inicio = () => {
   return (
-    <div className="md:grid md:grid-cols-2 justify-center gap-40  pt-44 pb-60 ml-28 mr-28">
+    <main className="md:grid md:grid-cols-2 justify-center gap-40  pt-44 pb-60 ml-28 mr-28">
       <InicioLeft />
       <InicioRight />
-    </div>
+    </main>
   )
 }
 
