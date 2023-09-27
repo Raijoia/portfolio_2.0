e0e8d28a592bc41c52314f8aaba524aca33a2282
Rai Joia <megac154@gmail.com>
2023-09-27 20:14:55 -0300

    navBar with label ok (bug)

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,25 +1,35 @@
 import { Coffee, Computer, HomeOutlined } from "@mui/icons-material"
 import FolderCopyIcon from "@mui/icons-material/FolderCopy"
 import { BottomNavigation, BottomNavigationAction } from "@mui/material"
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 const NavBar = () => {
+  const navigate = useNavigate()
+
   return (
     <header className="flex justify-around alignItens-center items-center pt-5 ">
       <h1 className="hidden sm:block font-mono">Olá, eu sou o Raí Joia</h1>
       <BottomNavigation showLabels>
-        <Link to='/' >
-          <BottomNavigationAction label="Home" icon={<HomeOutlined />} /> 
-        </Link>
-        <Link to='/sobremim'>
-          <BottomNavigationAction label="Sobre Mim" icon={<Coffee />} />
-        </Link>
-        <Link to='/formacao'>
-          <BottomNavigationAction label="Formação" icon={<Computer />} />
-        </Link>
-        <Link to='/projetos'>
-          <BottomNavigationAction label="Projetos" icon={<FolderCopyIcon />} />
-        </Link>
+        <BottomNavigationAction
+          label="Home"
+          icon={<HomeOutlined />}
+          onClick={() => navigate("/")}
+        />
+        <BottomNavigationAction
+          label="Sobre Mim"
+          icon={<Coffee />}
+          onClick={() => navigate("/sobremim")}
+        />
+        <BottomNavigationAction
+          label="Formação"
+          icon={<Computer />}
+          onClick={() => navigate("/formacao")}
+        />
+        <BottomNavigationAction
+          label="Projetos"
+          icon={<FolderCopyIcon />}
+          onClick={() => navigate("/projetos")}
+        />
       </BottomNavigation>
     </header>
   )
