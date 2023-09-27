b2d98c233c6800b71deadb87e3a962ba8a686a79
Rai Joia <megac154@gmail.com>
2023-09-27 20:57:12 -0300

    page AboutMe parcial ok

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,11 +1,17 @@
 import InicioRight from "../InicioRight"
+import TextAboutMe from "../TextAboutMe"
 
 const AboutMe = () => {
   return (
     <>
-      <h3 className="text-center text-3xl" >Sobre mim</h3>
-      <main className="md:grid md:grid-cols-2 justify-center gap-40  pt-44 pb-60 ml-28 mr-28">
-        <InicioRight />
+      <h3 className="text-center text-3xl mt-8" >Sobre mim</h3>
+      <main className="md:grid md:grid-cols-2 justify-center items-center gap-40  pt-44 pb-60 ml-28 mr-28">
+        <div className="ml-10">
+          <InicioRight />
+        </div>
+        <div className="mr-32">
+          <TextAboutMe />
+        </div>
       </main>
     </>
   )
---
+++ src/components/TextAboutMe/index.tsx
@@ -0,0 +1,12 @@
+const TextAboutMe = () => {
+  return (
+    <p>
+      Eu sou um Desenvolvedor front-end com conhecimentos em HTML, CSS, JavaScript,
+      React, TypeScript, Java e Git. Apaixonado por criar sites e aplicativos
+      web dinâmicos, responsivos e interativos, utilizando as melhores práticas
+      e ferramentas do mercado
+    </p>
+  )
+}
+
+export default TextAboutMe
