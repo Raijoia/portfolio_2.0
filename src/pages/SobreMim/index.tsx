82e19b55a88a1876c3745b71a2e5f50561185833
Rai Joia <megac154@gmail.com>
2023-10-03 16:08:00 -0300

    refactor SobreMim

--- src/pages/SobreMim/index.tsx
+++ src/pages/SobreMim/index.tsx
@@ -1,8 +1,20 @@
-import AboutMe from "./AboutMe"
+import Image from '../../components/Image'
+import Title from "../../components/Title"
+import TextAboutMe from "./TextAboutMe"
 
 const SobreMim = () => {
   return (
-    <AboutMe />
+    <>
+      <Title>Sobre Mim</Title>
+      <main className="md:grid md:grid-cols-2 justify-center items-center gap-40  pt-44 pb-60 ml-28 mr-28">
+        <div className="ml-10">
+          <Image />
+        </div>
+        <div className="mr-32">
+          <TextAboutMe />
+        </div>
+      </main>
+    </>
   )    
 }
 
