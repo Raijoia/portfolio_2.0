afb82bd073947ee0b71f2cc8beed2aed31d7562a
Rai Joia <megac154@gmail.com>
2023-10-03 16:04:28 -0300

    refactor h3 in aboutme

--- src/pages/SobreMim/AboutMe/index.tsx
+++ src/pages/SobreMim/AboutMe/index.tsx
@@ -1,10 +1,11 @@
 import TextAboutMe from "../TextAboutMe"
 import Image from "../../../components/Image";
+import Title from "../../../components/Title";
 
 const AboutMe = () => {
   return (
     <>
-      <h3 className="text-center text-3xl mt-8">Sobre mim</h3>
+      <Title>Sobre Mim</Title>
       <main className="md:grid md:grid-cols-2 justify-center items-center gap-40  pt-44 pb-60 ml-28 mr-28">
         <div className="ml-10">
           <Image />
