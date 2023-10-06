35f194d0ad7394001abc726bd9770f41173f535e
Rai Joia <megac154@gmail.com>
2023-10-06 17:39:40 -0300

    uuid add and projetos

--- src/pages/Projetos/index.tsx
+++ src/pages/Projetos/index.tsx
@@ -1,6 +1,38 @@
+import Title from "../../components/Title"
+import { v4 as uuidv4 } from 'uuid';
+
 const Projetos = () => {
+  const projetos = [
+    {
+      id: uuidv4(),
+      nome: "Organo",
+      vercel: "https://organo-with-typescript-tau.vercel.app/",
+      github: "https://github.com/Raijoia/organo_with_typescript",
+    },
+    {
+      id: uuidv4(),
+      nome: "Aluroni",
+      vercel: "https://aluroni-otimizado.vercel.app/",
+      github: "https://github.com/Raijoia/aluroni_otimizado",
+    },
+    {
+      id: uuidv4(),
+      nome: "AlFood",
+      vercel: "https://al-food-iota.vercel.app/",
+      github: "https://github.com/Raijoia/AlFood",
+    },
+    {
+      id: uuidv4(),
+      nome: "Timer pra estudo",
+      vercel: "https://time-for-tasks.vercel.app/",
+      github: "https://github.com/Raijoia/time_for_tasks",
+    },
+  ]
+
+  console.log(projetos)
+
   return (
-    <h5>Página em andamento</h5>
+    <Title>Projetos</Title>
   )
 }
 
