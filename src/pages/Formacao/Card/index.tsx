b4a05300be070197999dd2947cc135947342018d
Rai Joia <megac154@gmail.com>
2023-10-06 16:37:48 -0300

    center formation and delete dark

--- src/pages/Formacao/Card/index.tsx
+++ src/pages/Formacao/Card/index.tsx
@@ -4,7 +4,7 @@ const Card = ( { id, curso, instituicao, periodo } :ICard) => {
   return (
     <li
       key={id}
-      className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex gap-2 flex-col items-center sm:hover:shadow-gray-500 sm:hover:shadow-lg font-sans"
+      className="p-5 bg-gray-200 sm:rounded-xl sm:shadow-lg flex gap-2 flex-col items-center sm:hover:shadow-gray-500 sm:hover:shadow-lg font-sans"
     >
       <h4 className="font-bold">{curso}</h4>
       <p className="text-center">
