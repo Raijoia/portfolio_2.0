39ef832b60d4ce1920b565330dc2cb2805794a88
Raí Joia <megac154@gmail.com>
2024-01-02 23:50:40 -0300

    att page sobreMim

--- src/pages/SobreMim/TextAboutMe/index.tsx
+++ src/pages/SobreMim/TextAboutMe/index.tsx
@@ -1,8 +1,7 @@
 const TextAboutMe = () => {
   return (
     <p>
-      Eu sou um Desenvolvedor front-end com conhecimentos em HTML, CSS, JavaScript,
-      React, TypeScript, Java e Git. Apaixonado por criar sites e aplicativos
+      Eu sou um Desenvolvedor full stack com conhecimentos emNext, React, TypeScript, JavaScript, NodeJS, MongoDB, Express, Java, SpringBoot, MySQL, Tailwind, Git e SASS. Apaixonado por criar sites e aplicativos
       web dinâmicos, responsivos e interativos, utilizando as melhores práticas
       e ferramentas do mercado
     </p>
