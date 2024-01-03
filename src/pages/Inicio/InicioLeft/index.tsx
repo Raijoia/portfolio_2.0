332f135e0af06daf3bf2411c87d07ee8f2cefff6
Raí Joia <megac154@gmail.com>
2024-01-02 23:49:20 -0300

    att page inicio

--- src/pages/Inicio/InicioLeft/index.tsx
+++ src/pages/Inicio/InicioLeft/index.tsx
@@ -1,33 +1,34 @@
 import { Button } from "@mui/material"
+import { v4 as uuidv4 } from "uuid"
 
 const InicioLeft = () => {
   const sociais = [
     {
       link: "https://github.com/Raijoia",
       name: "GitHub",
-      id: 1,
+      id: uuidv4(),
     },
     {
       link: "https://www.linkedin.com/in/raijoia/",
       name: "Linkedin",
-      id: 2,
+      id: uuidv4(),
     },
     {
       link: "https://api.whatsapp.com/send?phone=5511977733585",
       name: "WhatsApp",
-      id: 3
+      id: uuidv4()
     }
   ]
 
   return (
     <div className="flex flex-col items-center justify-center">
       <h2 className="text-4xl text-center">
-        Raí Joia <br /> Desenvolvedor Front-end
+        Raí Joia <br /> Desenvolvedor Full Stack
       </h2>
       <p className="ml-6">
-        Eu sou um desenvolvedor front-end com especialidades em{" "}
+        Eu sou um desenvolvedor full stack com especialidades em{" "}
         <b className="font-bold text-center">
-          React, TypeScript, JavaScript, Git e SASS
+          Next, React, TypeScript, JavaScript, NodeJS, MongoDB, Express, Java, SpringBoot, MySQL, Tailwind, Git e SASS
         </b>
       </p>
       <div className="flex gap-5 lg:gap-10 mt-5">
